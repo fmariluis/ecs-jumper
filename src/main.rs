40d8f247160a38f4ddba92b512530dab6895b44a
Franco Mariluis <fmariluis@gmail.com>
2024-09-05 18:21:16 -0300

    improve code

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,61 @@
 #![allow(clippy::result_large_err)]
 
 use aws_config::meta::region::RegionProviderChain;
-use aws_sdk_ecs::{config::BehaviorVersion, config::Region, meta::PKG_VERSION, Error};
+use aws_sdk_ecs::{config::BehaviorVersion, config::Region, Error};
 use clap::Parser;
 
 const DEFAULT_REGION: &str = "us-west-2";
 
 #[derive(Debug, Parser)]
+#[clap(
+    name = "ecs-connector",
+    about = "Generate ECS Fargate connection strings"
+)]
 struct Opt {
-    /// The AWS Region. Defaults to us-west-2
-    #[structopt(short, long, default_value = "us-west-2")]
-    region: Option<String>,
+    /// The AWS Region.
+    #[clap(short, long, default_value = DEFAULT_REGION)]
+    region: String,
 
     /// The ECS cluster name
-    #[structopt(short, long, required = true)]
+    #[clap(short, long)]
     cluster: String,
 
     /// The ECS service name
-    #[structopt(short, long, required = true)]
+    #[clap(short, long)]
     service_name: String,
 
     /// The ECS task container name
-    #[structopt(long, short='n', required = true)]
+    #[clap(short = 'n', long)]
     container_name: String,
 
     /// Whether to display additional information.
-    #[structopt(short, long)]
+    #[clap(short, long)]
     verbose: bool,
 }
 
-// returns a connection string suitable for remotely connecting to a Fargate task
 async fn get_fargate_connection_string(
     client: &aws_sdk_ecs::Client,
-    cluster: String,
-    container_name: String,
-    service_name: String,
-) -> Result<(), aws_sdk_ecs::Error> {
-
+    cluster: &str,
+    container_name: &str,
+    service_name: &str,
+    region: &str,
+) -> Result<(), Error> {
     let tasks = client
         .list_tasks()
-        .set_cluster(Option::from(cluster.clone()))
-        .set_service_name(service_name.into())
+        .cluster(cluster)
+        .service_name(service_name)
         .send()
         .await?;
 
     for task_arn in tasks.task_arns() {
         let command = format!(
-            r#"aws ecs execute-command  \
-        --region {region} \
-        --cluster {cluster} \
-        --task {task_arn} \
-        --container {container_name} \
-        --command "/bin/bash" \
-        --interactive
-    "#,
-            region = "us-west-2",
-            cluster = cluster,
-            container_name = container_name,
-            task_arn = task_arn,
+            r#"aws ecs execute-command \
+            --region {region} \
+            --cluster {cluster} \
+            --task {task_arn} \
+            --container {container_name} \
+            --command "/bin/bash" \
+            --interactive"#
         );
         println!("{}", command);
     }
@@ -67,26 +65,20 @@ async fn get_fargate_connection_string(
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let Opt {
-        region,
-        verbose,
-        cluster,
-        container_name,
-        service_name,
-    } = Opt::parse();
+    let opt = Opt::parse();
 
-    let region_provider = RegionProviderChain::first_try(region.clone().map(Region::new))
+    let region_provider = RegionProviderChain::first_try(Region::new(opt.region.clone()))
         .or_default_provider()
         .or_else(Region::new(DEFAULT_REGION));
 
-    if verbose {
+    if opt.verbose {
         println!();
-        println!("ECS client version: {}", PKG_VERSION);
+        println!("ECS client version: {}", env!("CARGO_PKG_VERSION"));
         println!(
             "Region:             {}",
             region_provider.region().await.unwrap().as_ref()
         );
-        println!("Cluster:            {}", cluster);
+        println!("Cluster:            {}", opt.cluster);
         println!();
     }
 
@@ -96,6 +88,12 @@ async fn main() -> Result<(), Error> {
         .await;
     let client = aws_sdk_ecs::Client::new(&shared_config);
 
-    // show_clusters(&client).await
-    get_fargate_connection_string(&client, cluster, container_name, service_name).await
+    get_fargate_connection_string(
+        &client,
+        &opt.cluster,
+        &opt.container_name,
+        &opt.service_name,
+        &opt.region,
+    )
+    .await
 }
